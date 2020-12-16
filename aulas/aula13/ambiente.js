43ff113f899574f1d02eb7e586471c73ee38408a
Lucas Arcanjo <61436988+lucas-arcanjo@users.noreply.github.com>
2020-12-16 12:17:40 -0300

    aula13

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,18 @@
+/*c = 1
+while (c <= 6) {
+    console.log('Olá')
+    c++
+}*/
+
+c = 1
+do {
+    console.log(`testando ${c}x`)
+    c++
+} while (c <= 6)
+
+/*console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')*/
