3fec1fe718dcf8191898f4405f4fa684148c7e31
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-21 21:22:17 -0300

    .

--- aulas/aula14exe/ex016/script.js
+++ aulas/aula14exe/ex016/script.js
@@ -1,1 +1,9 @@
-console.log('teste')
+var inicio = document.getElementById('txtinicio')
+var fim = document.querySelector('div#txtfim')
+var res = document.querySelector('div#res')
+function contar() {
+    for (inicio = 1; c <= 10; c += 2) {
+        console.log(`${c}x`)
+        res.innerHTML = `a soma de ${inicio} é essa`
+    }
+}
