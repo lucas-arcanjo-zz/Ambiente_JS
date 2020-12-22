b0afca770722c61354eb6c4f31a12e8e705a307b
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-22 10:32:25 -0300

    finalizando ex016

--- aulas/aula14exe/ex016/script.js
+++ aulas/aula14exe/ex016/script.js
@@ -3,9 +3,29 @@ function contar() {
     let fim = document.getElementById('txtfim')
     let pas = document.querySelector('input#txtpasso')
     let res = document.getElementById('res')
+
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('[ERRO] faltam dados')
+        //window.alert('[ERRO] faltam dados')
+        res.innerHTML = 'Impossivel contar!'
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        if (p == 0) {
+            window.alert('Passo inválido, considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } res.innerHTML += `\u{1F3C1}`
     }     
 }
