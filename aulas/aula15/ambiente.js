6f8efde257997e11188ec4ff480ab08e8ed64f1f
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-23 17:40:44 -0300

    utilizando indexOf para encontrar elementos

--- aulas/aula15/ambiente.js
+++ aulas/aula15/ambiente.js
@@ -1,5 +1,4 @@
 let num = [ 2, 6, 3 ]
-console.log(num)
 num.sort()
 num.push(1)
 console.log(num)
@@ -7,3 +6,10 @@ console.log(`O vetor tem ${num.length} elementos!`)
 num.sort()
 console.log(num)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(10)
+if (pos == -1) {
+    console.log('Valor não encontrado')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
