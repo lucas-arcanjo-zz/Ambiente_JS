76f1afc8385cd52c761ccd0ff25c8d7e98e1d0de
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-23 17:19:55 -0300

    usando vetores com estrutura de repetição for

---
+++ aulas/aula15/VetorNaTela.js
@@ -0,0 +1,11 @@
+let valores = [4, 6, 2, 3, 1]
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+*/
+for (pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
