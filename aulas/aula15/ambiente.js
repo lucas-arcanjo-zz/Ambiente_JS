553dfc00efec14437212d6669f3690a07d571336
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-23 16:53:25 -0300

    ensino de vetores

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [ 2, 6, 3 ]
+console.log(num)
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} elementos!`)
+num.sort()
+console.log(num)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
