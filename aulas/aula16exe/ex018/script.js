8ba46b1b9bcebb8c27a1130cacda8ef217ed6f2b
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-24 22:33:40 -0300

    fase1ok

---
+++ aulas/aula16exe/ex018/script.js
@@ -0,0 +1,8 @@
+function adicionar() {
+    let cx = document.getElementById('txtn')
+    if (cx.value <= 0 || cx.value > 100) {
+        window.alert('Valor inválido ou já encontrado na lista')
+    } else {
+        
+    }
+} 
