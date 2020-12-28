fc03d12d1bcc1b00ff0c8731e29e05472274fa81
lucas-arcanjo <lucas.saolliveira@gmail.com>
2020-12-28 18:24:20 -0300

    cont

--- aulas/aula16exe/ex018/script.js
+++ aulas/aula16exe/ex018/script.js
@@ -1,12 +1,19 @@
 let cax = document.getElementById('txtn')
+let res = document.getElementById('res')
+let selcx =  document.getElementById('cxn')
+
 function adicionar() {
     if (cax.value <= 0 || cax.value > 100) {
         window.alert('Valor inválido ou já encontrado na lista')
     } else {
-        cx = Number(cax.value)
+        //selcx.innerHTML= ''
+        let cx = Number(cax.value)
         let item = document.createElement('option')
-        
-         
-        
+        item.text = `valor ${cx} adicionado`
+        selcx.appendChild(item)
     }
 } 
+
+function finalizar() {
+    return res.innerHTML = 'ok'
+}
