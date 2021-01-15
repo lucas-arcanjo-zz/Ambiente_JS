8eea727a92fbffc8ea1e3e077ad2fef0ef3a0f27
lucas-arcanjo <lucas.saolliveira@gmail.com>
2021-01-14 23:15:55 -0300

    remoção de ;

--- treinos/amb1/poo1.js
+++ treinos/amb1/poo1.js
@@ -1,7 +1,7 @@
 function createNewPerson(name) {
-    var obj = {};
-    obj.name = name;
-    console.log('Hi! I\'m ' + obj.name + '.');
-    return obj;
+    var obj = {}
+    obj.name = name
+    console.log('Hi! I\'m ' + obj.name + '.')
+    return obj
 }
 createNewPerson('lucas')
