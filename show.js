3b2d2661a45c7ca46833745c1dddf24c89c08107
lucas-arcanjo <lucas.saolliveira@gmail.com>
2021-01-26 00:02:48 -0300

    ,

---
+++ show.js
@@ -0,0 +1,13 @@
+/*console.log("E aí cara, vai ou não vai aprender essa josa?")
+console.log("Vamos tentar...")
+for(c = 1; c <= 10; c++) {
+    console.log(`tentando ${c}vez(es)` )
+}
+tab = 9
+for (c = 0; c <= 10; c++) {
+    console.log(`${tab} x ${c} = ${c*tab}`)
+}
+console.log("FIM")*/
+ale = Math.floor(91, 11)
+console.log(ale)
+
