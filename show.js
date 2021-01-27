b3e3759cad3cd6eaf0817712869694f104f04d1e
lucas-arcanjo <lucas.saolliveira@gmail.com>
2021-01-27 18:44:35 -0300

    exer tabuada

--- show.js
+++ show.js
@@ -1,12 +1,5 @@
-/*console.log("E aí cara, vai ou não vai aprender essa josa?")
-console.log("Vamos tentar...")
-for(c = 1; c <= 10; c++) {
-    console.log(`tentando ${c}vez(es)` )
+let tab = 12
+for (c = 1; c <= 10; c++) {
+    console.log(`${tab} X  ${c} = ${c*tab}`)
 }
-tab = 9
-for (c = 0; c <= 10; c++) {
-    console.log(`${tab} x ${c} = ${c*tab}`)
-}
-console.log("FIM")*/
-ale = Math.floor(91, 11)
-console.log(ale)
+console.log('Fim')
