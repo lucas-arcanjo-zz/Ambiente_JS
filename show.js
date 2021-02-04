d64169ec66aad1062747e15bc8b7a6bd45e923de
lucas-arcanjo <lucas.saolliveira@gmail.com>
2021-02-04 17:02:53 -0300

    utilizando arrow function

--- show.js
+++ show.js
@@ -1,5 +1,27 @@
-let tab = 12
-for (c = 1; c <= 10; c++) {
-    console.log(`${tab} X  ${c} = ${c*tab}`)
+/* 
+    * function simpls
+        function somar(soma1, soma2) {
+            return soma1 + soma2;
+        }
+        console.log(somar(5, 10))
+==================================================
+    * arrow function
+        var somar = (soma1, soma2) {
+            return soma1 + soma2
+        }
+        console.log(somar(5, 10))
+==================================================
+    * arrow function         
+
+
+*/
+
+
+
+var soma = (n1, n2) => {
+    return n1 + n2
 }
-console.log('Fim')
+
+
+
+console.log(soma(1, 20));
