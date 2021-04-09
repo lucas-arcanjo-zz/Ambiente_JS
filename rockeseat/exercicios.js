ceeba6fca0e145d85678b360303715564ff854c8
Lucas Arcanjo <lucas.saolliveira@gmail.com>
2021-04-09 09:46:44 -0300

    Buscando e contando dadas em Arrays

--- rockeseat/exercicios.js
+++ rockeseat/exercicios.js
@@ -223,3 +223,89 @@ try {
 }
 // OBS: PODE HAVER UM NaN se o argumento da função for '10ef', por isso, é necessário limpar, usando a expressão regular.
 // ===================================== */
+
+/*// Buscando e contando dadas em Arrays
+
+    // Baseado no Array de Livros por Categoria abaixo,  faça os seguintes desafios
+
+        // Contar o número de categorias e o número de livros em categoria
+        // Contar o número de autores
+        // Mostrar livros do autor Augusto Cury
+        // Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor.
+
+// ### Exercicio corrigido
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico de Babilonia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - Como enfretntar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+        ],
+    },
+];
+
+const countCategory = booksByCategory.length
+
+console.log(`Existem ${countCategory} categoria(s)`)
+for (let category of booksByCategory) {
+    console.log("Total de livros da categoria: ", category.category)
+    console.log(category.books.length)
+}
+
+function countBooks() {
+    let authors = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if (authors.indexOf(book.author) == -1) {
+                authors.push(book.author)
+            }
+        }
+    }
+    console.log(`Total de autores: ${authors.length}`)
+}
+
+countBooks();
+
+function bookOfAuthor(author) {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+    console.log(`Livros do autor ${author}: ${books.join(", ")}`)
+}
+
+bookOfAuthor("Augusto Cury")
+*/
